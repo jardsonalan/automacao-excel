46f569889c797a10b4dd274212e8757e0479686c
Jardson Alan <jardsonalan132@gmail.com>
2025-09-13 20:02:12 -0300

    feat: Funções para tratar os dados da planilha

---
+++ src/data_cleaner.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def rename_headers(df: pd.DataFrame) -> pd.DataFrame:
+  return df.rename(columns={
+    'Compound': 'Ôxidos',
+    'm/m%': '%',
+    'm/m%.1': '%%',
+    'El': 'Elementos'
+  })
+
+def correction_datas(df: pd.DataFrame) -> pd.DataFrame:
+  if 'Elementos' in df.columns:
+    df['Elementos'] = df['Elementos'].str.strip().replace({
+      'Sx': 'S',
+      'Px': 'P'
+    })
+
+  return df
