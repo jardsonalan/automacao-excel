ae9416f3949bc6fb78f9855d0d483a549bf2f612
Jardson Alan <jardsonalan132@gmail.com>
2025-09-13 23:34:59 -0300

    feat: Adicionando a chamada da função de formatação

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from data_loader import load_excel
 from data_cleaner import rename_headers, correction_datas
 from excel_utils import filter_datas, save_to_excel
+from excel_formatter import format_excel
 
 def main():
-  df = load_excel('./data/raw/AL-E2.xls')
-  rename = rename_headers(df)
-  correct_datas = correction_datas(rename)
-  filters = filter_datas(correct_datas)
-  save_to_excel('AL-E2-tratado', filters)
+  input_path = './data/raw/AL-E2.xls'
+  # Carrega a planilha
+  df = load_excel(input_path)
+  # Renomeia o cabeçalho
+  df = rename_headers(df)
+  # Corrije os dados errados
+  df = correction_datas(df)
+  # Filtra os dados necessários
+  df = filter_datas(df)
+  # Salva em Excel, após o tratamento dos dados (sem formatação ainda)
+  processed_path = save_to_excel(df, 'AL-E2-tratado')
+
+  # Aplica a formatação visual
+  format_excel(processed_path)
+
   print('Processo concluído com sucesso.')
 
 if __name__ == "__main__":
