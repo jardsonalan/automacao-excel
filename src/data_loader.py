cf13de825e9e68371adad3ec9e44d02253cc19ab
Jardson Alan <jardsonalan132@gmail.com>
2025-09-14 20:48:09 -0300

    fix: Ajuste no parâmetro header da função de carregamento do Excel

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-def load_excel(path: str, header=16, nrows=15) -> pd.DataFrame:
-  return pd.read_excel(path, sheet_name=0, header=header, nrows=nrows)
+def load_excel(path: str, header: int = 17, nrows: int = 15) -> pd.DataFrame:
+  return pd.read_excel(path, sheet_name=0, header=header-1, nrows=nrows)
