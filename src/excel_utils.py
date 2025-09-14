140ebc77acf5c56aeda4cf1b9ac89666fc3a56f3
Jardson Alan <jardsonalan132@gmail.com>
2025-09-13 23:32:28 -0300

    refactor: Melhoria na verificação da pasta de salvamento e na verificação da extensão do arquivo

--- src/excel_utils.py
+++ src/excel_utils.py
@@ -1,8 +1,24 @@
 import pandas as pd
+import os
 
 def filter_datas(df: pd.DataFrame) -> pd.DataFrame:
   return df[['Ôxidos', '%', 'Elementos', '%%']]
 
-def save_to_excel(name: str, datas_filter: pd.DataFrame, sheet_name="Table") -> str:
-  datas_filter.to_excel('./data/processed/'+name+'.xlsx', index=False, sheet_name=sheet_name)
-  return 'Nova tabela criada.'
+def save_to_excel(datas_filter: pd.DataFrame,
+                  name: str,
+                  folder: str = './data/processed',
+                  sheet_name: str = 'Tabela') -> str:
+  
+  # Serve para garantir que a pasta (folder) existe
+  os.makedirs(folder, exist_ok=True)
+
+  # Garante a extensão (.xlsx)
+  if not name.endswith('.xlsx'):
+    name += '.xlsx'
+  
+  path = os.path.join(folder, name)
+
+  # Salva o Dataframe
+  datas_filter.to_excel(path, index=False, sheet_name=sheet_name)
+
+  return path
