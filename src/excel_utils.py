76cd26752394393b8d95385ddd07043e4783bc46
Jardson Alan <jardsonalan132@gmail.com>
2025-09-14 21:14:41 -0300

    refactor: Remoção de validações desnecessárias

--- src/excel_utils.py
+++ src/excel_utils.py
@@ -7,14 +7,6 @@ def filter_datas(df: pd.DataFrame) -> pd.DataFrame:
 def save_to_excel(datas_filter: pd.DataFrame,
                   path: str,
                   sheet_name: str = 'Tabela') -> str:
-
-  # Garante a extensão (.xlsx)
-  if not path.endswith('.xlsx'):
-    path += '.xlsx'
-  
-  os.makedirs(os.path.dirname(path), exist_ok=True)
-
   # Salva o Dataframe
   datas_filter.to_excel(path, index=False, sheet_name=sheet_name)
-
   return path
