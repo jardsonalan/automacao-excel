ef39864f8d16a16c1475c6d985135ff0e5bc88a7
Jardson Alan <jardsonalan132@gmail.com>
2025-09-15 20:32:41 -0300

    style: Alterando a interface do usuário com ajustes no layout e estilo dos componentes

--- src/main.py
+++ src/main.py
@@ -60,41 +60,80 @@ def run_process():
 def main():
   global entry, entry_header, entry_nrows
 
+  # Janela da aplicação
   root = tk.Tk()
   root.title('Automação Excel')
-  root.geometry('500x500')
+  root.geometry('500x250')
+  root.configure(bg="#E9E9E9")
 
-  entry = tk.Entry(root, width=60)
-  entry.pack(pady=10)
+  # Campo de texto
+  entry = tk.Entry(root, width=50, font=('Arial', 13), relief='flat')
+  entry.pack(pady=10, padx=10)
 
-  btn_select = tk.Button(root, text='Selecionar uma planilha', command=select_file)
-  btn_select.pack(pady=5)
+  # Botão de seleção de planilha
+  btn_select = tk.Button(root,
+                         text='Selecionar uma planilha',
+                         command=select_file,
+                         font=('Arial', 12),
+                         cursor='hand2',
+                         bg="#FF9100",
+                         activebackground="#FFA227",
+                         fg='black',
+                         relief='flat',
+                         width=50)
+  btn_select.pack(pady=10, padx=10)
 
+  # Opção de linha do cabeçalho
   frame_header = tk.Frame(root)
-  frame_header.pack(pady=5)
-  tk.Label(frame_header, text="Linha do cabeçalho:").pack(side=tk.LEFT, padx=5)
-  entry_header = tk.Spinbox(frame_header, from_=0, to=100, width=5)
+  frame_header.pack(pady=10)
+  frame_header.configure(bg='#E9E9E9')
+  tk.Label(frame_header,
+           text="Linha do cabeçalho:",
+           font=('Arial', 12),
+           bg='#E9E9E9').pack(side=tk.LEFT, padx=10)
+  entry_header = tk.Spinbox(frame_header,
+                            from_=0,
+                            to=100,
+                            width=50,
+                            font=('Arial', 12),
+                            justify='center')
   entry_header.delete(0, tk.END)
   entry_header.insert(0, "17")
-  entry_header.pack(side=tk.LEFT)
+  entry_header.pack(side=tk.LEFT, padx=10)
 
+  # Opção de quantidade de dados
   frame_nrows = tk.Frame(root)
-  frame_nrows.pack(pady=5)
-  tk.Label(frame_nrows, text="Quantidade de dados:").pack(side=tk.LEFT, padx=5)
-  entry_nrows = tk.Spinbox(frame_nrows, from_=1, to=1000, width=5)
+  frame_nrows.pack(pady=10)
+  frame_nrows.configure(bg='#E9E9E9')
+  tk.Label(frame_nrows,
+           text="Quantidade de dados:",
+           font=('Arial', 12),
+           bg='#E9E9E9').pack(side=tk.LEFT, padx=10)
+  entry_nrows = tk.Spinbox(frame_nrows,
+                           from_=1,
+                           to=1000,
+                           width=50,
+                           font=('Arial', 12),
+                           justify='center')
   entry_nrows.delete(0, tk.END)
-  entry_nrows.insert(0, "15")  # valor padrão
-  entry_nrows.pack(side=tk.LEFT)
+  entry_nrows.insert(0, "15")
+  entry_nrows.pack(side=tk.LEFT, padx=10)
 
+  # Botão para rodar aplicação
   btn_run = tk.Button(
     root,
     text='Rodar Automação',
     command=run_process,
-    bg='green',
+    bg="#00750A",
+    activebackground="#279731",
     fg='white',
-    font=('Arial', 12, 'bold')
+    activeforeground='white',
+    font=('Arial', 12, 'bold'),
+    cursor='hand2',
+    relief='flat',
+    width=50
   )
-  btn_run.pack(pady=20)
+  btn_run.pack(pady=10)
 
   root.mainloop()
 
